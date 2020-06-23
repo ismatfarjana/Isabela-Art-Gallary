98e41fff43eed6d679891f97992b4865c19566c6
Syeda Ismat Farjana <diptyfmc17@gmail.com>
2020-06-23 10:51:03 +1000

    added pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/', to: 'arts#index'
   get '/about', to: 'arts#about'
+  get '/contact', to: 'arts#contact'
+  get '/collection', to: 'arts#collection'
 end
