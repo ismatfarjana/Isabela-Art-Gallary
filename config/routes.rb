bb2468ea997c31fcac761394bfb91c995c53d174
Syeda Ismat Farjana <diptyfmc17@gmail.com>
2020-06-23 21:00:46 +1000

    add devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'arts#index'
   get '/', to: 'arts#index'
   get '/about', to: 'arts#about'
   get '/contact', to: 'arts#contact'
